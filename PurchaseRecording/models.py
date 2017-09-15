bb823111e5f763ad3fd21d669b1cb45ea9c3cdb9
zsdc0306 <zsdc0306@gmail.com>
2017-09-15 02:01:01 -0400

    ad key to address DB

--- PurchaseRecording/models.py
+++ PurchaseRecording/models.py
@@ -24,19 +24,21 @@ class CreditCard(models.Model):
     card_num = models.CharField(unique=True, primary_key=True, max_length=16)
     sec_code = models.CharField(max_length=4, default="0000")
     card_name = models.CharField(unique=True, max_length=10)
-    card_exp_date = models.DateField(datetime.now().date()+timedelta(days=365*5))
+    card_exp_date = models.DateField(datetime.now().date() + timedelta(days=365 * 5))
+
     def __unicode__(self):
         return self.card_name
 
 
 class CashBackSite(models.Model):
     site_url = models.CharField(unique=True, max_length=20, error_messages={'unique': "site already existed"})
-    site_name = models.CharField(unique=True, primary_key=True, max_length= 10)
+    site_name = models.CharField(unique=True, primary_key=True, max_length=10)
 
 
 class Merchandiser(models.Model):
     name = models.CharField(primary_key=True, max_length=10)
     url = models.CharField(max_length=20)
+
     def __unicode__(self):
         return self.name
 
@@ -48,6 +50,7 @@ class Order(models.Model):
     order_date = models.DateField()
     order_status = models.IntegerField(choices=order_status_type, default=0)
     order_price = models.FloatField(default=0.0)
+
     def __unicode__(self):
         return self.order_name
 
@@ -59,13 +62,20 @@ class EarningDetail(models.Model):
     other_cashback = models.FloatField(default=0.0)
     note = models.CharField(blank=True, max_length=200)
     earing_num = models.FloatField(default=0)
+
     def __unicode__(self):
         return self.order_id.order_name
 
 
 class Address(models.Model):
-    addr_name = models.CharField(max_length=10)
+    addr_name = models.CharField(max_length=20)
     addr_zip = models.CharField(max_length=10)
+    addr_line1 = models.CharField(max_length=50, blank=True)
+    addr_line2 = models.CharField(max_length=50, blank=True)
+    addr_city = models.CharField(max_length=20, blank=True)
+    addr_state = models.CharField(max_length=10, blank=True)
+    is_default = models.BooleanField(default=False)
+
     def __unicode__(self):
         return self.addr_name
 
@@ -75,11 +85,12 @@ class OrderDetail(models.Model):
     order_name = models.CharField(max_length=20)
     item_price = models.FloatField(default=0.0)
     item_num = models.IntegerField(default=1)
-    carrier =models.IntegerField(choices=carrier_type,default=9)
-    tracking_num = models.CharField(max_length=20,default="undefined")
+    carrier = models.IntegerField(choices=carrier_type, default=9)
+    tracking_num = models.CharField(max_length=20, default="undefined")
     deliver_address = models.ForeignKey(Address)
     credit_card = models.ForeignKey(CreditCard)
     earning = models.ForeignKey(EarningDetail)
+
     def __unicode__(self):
         return self.order_name
 
@@ -98,4 +109,4 @@ class GiftCard(models.Model):
     pin_num = models.CharField(max_length=10)
     is_used = models.BooleanField()
     remain_value = models.FloatField(default=value)
-    expire_date = models.DateField(default=datetime.now().date()+timedelta(days=365))
+    expire_date = models.DateField(default=datetime.now().date() + timedelta(days=365))

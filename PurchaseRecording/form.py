fcbe8924d5a27626bd304f074c143d30ad6ec790
zsdc0306 <zsd0306@gmail.com>
2017-10-31 18:07:53 -0400

    gc management template using form to record

---
+++ PurchaseRecording/form.py
@@ -0,0 +1,34 @@
+from django import forms
+from models import *
+
+
+class GiftCardForm(forms.ModelForm):
+    class Meta:
+        model = GiftCard
+        fields = ['brand', 'value', 'card_num', 'pin_num', 'is_used', 'remain_value', 'expire_date']
+        widgets = {
+            'brand': forms.Select(attrs={
+                'class': 'form-control'
+            }),
+            'value': forms.NumberInput(attrs={
+                'class': 'form-control'
+            }),
+            'card_num': forms.TextInput(attrs={
+                'class': 'form-control'
+            }),
+            'pin_num': forms.NumberInput(attrs={
+                'class': 'form-control'
+            }),
+            'is_used': forms.CheckboxInput(attrs={
+                'class': 'form-control'
+            }),
+            'remain_value': forms.NumberInput(attrs={
+                'type': 'number',
+                'step': 'any',
+                'class': 'form-control'
+            }),
+            'expire_date': forms.DateInput(attrs={
+                'type': 'date',
+                'class': 'form-control'
+            })
+        }
